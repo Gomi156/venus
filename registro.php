fa95e75303a006f7c5f9777872a2101f160298d6
Gomi156 <hugo1996@gmail.com>
2022-11-29 10:43:07 -0600

    555

--- registro.php
+++ registro.php
@@ -1,5 +1,14 @@
 <?php
-include_once("db.php");
+//Obtener información de conexión de Heroku ClearDB
+$cleardb_url = parse_url ( getenv ( "CLEARDB_DATABASE_URL" ) ) ;  
+$cleardb_server = $cleardb_url [ "host" ] ;  
+$cleardb_username = $cleardb_url [ "usuario" ] ;  
+$cleardb_password = $cleardb_url [ "contraseña" ] ;  
+$cleardb_db = substr ( $cleardb_url [ "ruta" ] , 1 ) ;  
+$grupo_activo = 'predeterminado' ;  
+$query_builder = VERDADERO ;  
+// Conectar a la base de datos
+$conn = mysqli_connect ( $cleardb_server , $cleardb_username , $cleardb_password , $cleardb_db ) ; 
 
 $nombres=$_POST['nombres'];
 $apellidos=$_POST['apellidos'];

744d29a8497700f0c0b3ecab1c1270ff3a4b722c
Gomi156 <hugo1996@gmail.com>
2022-11-29 10:15:09 -0600

    9

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 function conn(){
-    $hostname = "localhost";
-    $usuario = "root";
-    $passworddb = "";
+    $hostname = "us-cdbr-east-06.cleardb.net";
+    $usuario = "ba0e4f86737330";
+    $passworddb = "b7cfe9df";
     $dbname = "heroku 15ce082b75bdfc1";
 
     $conectar = mysqli_connect($hostname, $usuario, $passworddb, $dbname);

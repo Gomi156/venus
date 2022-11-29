34a8a2a98698aa4c6ab1572708053f458ffe4b92
Gomi156 <hugo1996@gmail.com>
2022-11-29 10:39:03 -0600

    322

--- db.php
+++ db.php
@@ -1,15 +1,13 @@
 <?php
-function conn(){
-    $hostname = "us-cdbr-east-06.cleardb.net";
-    $usuario = "ba0e4f86737330";
-    $passworddb = "b7cfe9df";
-    $dbname = "heroku 15ce082b75bdfc1";
-
-    $conectar = mysqli_connect($hostname, $usuario, $passworddb, $dbname);
-    return $conectar;
-}
-
-
-
+//Obtener información de conexión de Heroku ClearDB
+$cleardb_url = parse_url ( getenv ( "CLEARDB_DATABASE_URL" ) ) ;  
+$cleardb_server = $cleardb_url [ "host" ] ;  
+$cleardb_username = $cleardb_url [ "usuario" ] ;  
+$cleardb_password = $cleardb_url [ "contraseña" ] ;  
+$cleardb_db = substr ( $cleardb_url [ "ruta" ] , 1 ) ;  
+$grupo_activo = 'predeterminado' ;  
+$query_builder = VERDADERO ;  
+// Conectar a la base de datos
+$conn = mysqli_connect ( $cleardb_server , $cleardb_username , $cleardb_password , $cleardb_db ) ;  
 
 ?>

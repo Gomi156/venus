a6a32f40d5485da2867e7414ce7eea530c7e2af7
Gomi156 <hugo1996@gmail.com>
2022-11-28 22:44:25 -0600

    2

--- registro.php
+++ registro.php
@@ -7,13 +7,15 @@ $correo=$_POST['correo'];
 $telefono=$_POST['telefono'];
 
 
-echo "Los datos son los siguintes: <br>";
-echo "$nombres, $apellidos, $correo, $telefono";
+
 
 
     $conectar=conn();
     $sql="INSERT INTO `usuarios` (`nombres`, `apellidos`, `correo`, `telefono`)
     VALUES ('$nombres', '$apellidos', '$correo','$telefono')";
     $resul = mysqli_query($conectar,$sql)or trigger_error("Query Failed! SQL- Error:".mysql_error($conectar));
-echo "$sql";
+ echo'<script type="text/javascript">
+alert("Tus datos han sido capturados");
+window.location.href="index.php";
+</script>';
 ?>

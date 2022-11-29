03534af8b150e6a3ffe5af6fcaaff0722593ca03
Gomi156 <hugo1996@gmail.com>
2022-11-28 23:02:18 -0600

    58

--- registro.php
+++ registro.php
@@ -15,7 +15,7 @@ $telefono=$_POST['telefono'];
     VALUES ('$nombres', '$apellidos', '$correo','$telefono')";
     $resul = mysqli_query($conectar,$sql)or trigger_error("Query Failed! SQL- Error:".mysql_error($conectar));
  echo'<script type="text/javascript">
-alert("Tus datos han sido capturados");
-window.location.href="";
+alert("Tarea Guardada");
+window.location.href="index.php";
 </script>';
 ?>
